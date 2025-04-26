c4d05e634b51411c7729413ddc5c4423665861c9
abok-cymk <allanabok18@gmail.com>
2025-04-26 10:07:24 +0300

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+const App = () => {
+  return (
+    <div className="text-2xl font-700 underline decoration-3 underline-offset-4 decoration-mySlate-900">
+      I love react, it's awesome
+    </div>
+  );
+}
+
+export default App;
